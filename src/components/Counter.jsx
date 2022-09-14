26e37bbe2d7ede61bcd92d2f5e8d1a73c14c9386
Your Name <you@example.com>
2022-09-14 13:23:12 +0200

    Toggle

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,11 +3,15 @@ import { useState } from 'react'
 
 export const Counter=()=> {
     const [counter,setCounter] = useState(0)
+    const handleMinus=() => {
+      if(counter>0)
+        setCounter(counter-1)
+    }
   return (
     <div className='d-flex justify-content-around w-50'>
-        <i className="fa-solid fa-plus fa-3x text-danger"></i>
+        <i className="fa-solid fa-plus fa-3x text-success" onClick={()=> setCounter(counter+1)}></i>
         <div>A számláló értéke: {counter}</div>
-        <i className="fa-solid fa-minus fa-3x text-success"></i>
+        <i className="fa-solid fa-minus fa-3x text-danger" onClick={handleMinus}></i>
     </div>
     
   )
--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Button } from 'react-bootstrap';
 
 export const Toggle=()=> {
+  const [isHappy,setIsHappy]=useState(true)
   return (
-    <div>ez a Toggle komponens</div>
+    <div className="d-flex justify-content-center mt-3">
+      <Button onClick={()=>setIsHappy(!isHappy)}>Are you happy? {isHappy?'😂':'😔'} </Button>
+    </div>
   )
 }
